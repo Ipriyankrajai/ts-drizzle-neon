7585844c3b5f8fc618c7622ceadf902ba9986215
Priyank Rajai <priyank73@hotmail.com>
2024-06-29 16:11:44 +0530

    Feat: adding enum and index to user table

--- src/schema/user.ts
+++ src/schema/user.ts
@@ -1,8 +1,26 @@
+import {
+  pgTable,
+  uuid,
+  varchar,
+  integer,
+  pgEnum,
+  index,
+} from "drizzle-orm/pg-core";
 
-import {pgTable, uuid ,varchar} from "drizzle-orm/pg-core"
+export const UserRole = pgEnum("user_role", ["ADMIN", "BASIC"]);
 
- export const UserTable = pgTable("user",{
+export const UserTable = pgTable(
+  "user",
+  {
     id: uuid("id").primaryKey().defaultRandom(),
-    firstName: varchar("first_name",{length:25}).notNull(),
-    lastName: varchar("last_name",{length:25}),
- })
+    name: varchar("name", { length: 25 }).notNull(),
+    age: integer("age").notNull(),
+    email: varchar("email", { length: 25 }).notNull().unique(),
+    role: UserRole("user_role").default("BASIC").notNull(),
+  },
+  (table) => {
+    return {
+      emailIndex: index("emailIndex").on(table.email),
+    };
+  }
+);
