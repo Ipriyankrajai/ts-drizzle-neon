f06b2045f4283819a217bb51d6bbe7e19bb7b6da
Priyank Rajai <priyank73@hotmail.com>
2024-06-29 16:13:37 +0530

    Feat: creating Post table and creating many to one relation with user table

---
+++ src/schema/post.ts
@@ -0,0 +1,11 @@
+import { pgTable, real, time, uuid, varchar } from "drizzle-orm/pg-core";
+import { UserTable } from "./user";
+
+export const PostTable = pgTable("post", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  title: varchar("title", { length: 255 }).notNull(),
+  averageRating: real("average_rating").notNull().default(0),
+  createdAt: time("created_at").defaultNow().notNull(),
+  updateddAt: time("updated_at").defaultNow().notNull(),
+  authorId: uuid("author_id").references(() => UserTable.id),
+});
